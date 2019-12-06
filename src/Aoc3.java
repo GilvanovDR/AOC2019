97c338228a07053719f745ccc0fad63cb0b6663f
lucefer13 <lucefer13+github@gmail.com>
2019-12-06 15:36:01 +0500

    AOC3_1 need fix TODO

--- src/Aoc3.java
+++ src/Aoc3.java
@@ -2,7 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.Math.abs;
 
 public class Aoc3 {
     public static void main(String[] args) throws IOException {
@@ -11,7 +14,7 @@ public class Aoc3 {
         String[] srcString2 = br.readLine().split(",");
         br.close();
         List<int[]> sections1 = getSections(srcString1);
-      /*  for (int[] ints : sections1) {
+/*        for (int[] ints : sections1) {
             for (int i : ints) {
                 System.out.print(i + " ");
             }
@@ -22,7 +25,49 @@ public class Aoc3 {
     }
 
     private static int getMinManhettn(List<int[]> sections1, List<int[]> sections2) {
-        return 0;
+        int minValue = Integer.MAX_VALUE;
+        int value;
+        for (int[] section1 : sections1) {
+            for (int[] section2 : sections2) {
+                int[] cross = getCross(section1, section2);
+                if (cross != null) {
+                    System.out.println(Arrays.toString(section1) + " "
+                            + Arrays.toString(section2) + " [" + cross[0] + ","
+                            + cross[1] + "]");
+                    if (cross[0] != 0 && cross[1] != 0) {
+                        value = abs(cross[0]) + abs(cross[1]);
+                        minValue = minValue < value ? minValue : value;
+                    }
+                }
+            }
+        }
+        return minValue;
+    }
+
+    private static int[] getCross(int[] section1, int[] section2) {
+        int crossX = 0;
+        int crossY = 0;
+        boolean flag = false;
+        //todo проверить условия пересечения (их 2 по вертикали и горизонтали (провереннот тока по гори)
+        if (section1[4] != section2[4]) {
+            if (section1[4] == 0) {
+                if ((section1[1] > section2[1]) && (section1[1] < section2[3])) {
+                    crossX = section1[1];
+                    crossY = section2[0];
+                    flag = true;
+                }
+            } else {
+                if ((section1[0] > section2[0]) && (section1[0] < section2[2])) {
+                    crossX = section1[0];
+                    crossY = section2[1];
+                    flag = true;
+                }
+            }
+        }
+        if (flag) {
+            return (new int[]{crossX, crossY});
+        }
+        return null;
     }
 
     private static List<int[]> getSections(String[] srcString) {
@@ -38,21 +83,38 @@ public class Aoc3 {
             startY = slideY;
             direct = aSrcString.charAt(0);
             countSteps = Integer.valueOf(aSrcString.substring(1));
+            int directNum = -1;
             switch (direct) {
                 case 'U':
                     slideY += countSteps;
+                    directNum = 1;
                     break;
                 case 'R':
                     slideX += countSteps;
+                    directNum = 0;
                     break;
                 case 'D':
                     slideY -= countSteps;
+                    directNum = 1;
                     break;
                 case 'L':
+                    directNum = 0;
                     slideX -= countSteps;
                     break;
             }
-            section.add(new int[]{startX, startY, slideX, slideY});
+            if (directNum == 0) {
+                if (startX < slideX) {
+                    section.add(new int[]{startX, startY, slideX, slideY, directNum});
+                } else {
+                    section.add(new int[]{slideX, startY, startX, slideY, directNum});
+                }
+            } else {
+                if (startY < slideY) {
+                    section.add(new int[]{startX, startY, slideX, slideY, directNum});
+                } else {
+                    section.add(new int[]{startX, slideY, slideX, startY, directNum});
+                }
+            }
         }
         return section;
     }
